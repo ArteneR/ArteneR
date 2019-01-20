f1ef36e4ab4631ad18948ca2f48d5806db3bfe24
ArteneR <georgevici.ioan@yahoo.com>
2019-01-20 17:00:42 +0200

    Stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopBannerComponent } from './main/top-banner/top-banner.component';
+import { AboutMeComponent } from './main/about-me/about-me.component';
+import { MySkillsComponent } from './main/my-skills/my-skills.component';
+import { HeaderComponent } from './main/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    TopBannerComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
