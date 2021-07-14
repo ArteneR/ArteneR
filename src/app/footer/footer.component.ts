7f20ecd613fb5973fc0b9e609325d023f5d92323
Mihai Georgevici <mihai@titlecapture.com>
2021-07-14 17:58:48 +0300

    Added change language buttons in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TranslateService } from '@ngx-translate/core';
+
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.less']
 })
 export class FooterComponent implements OnInit {
+    constructor(public translate: TranslateService) {}
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    getCurrentYear() {
+        return (new Date()).getFullYear();
+    }
 
-  getCurrentYear() {
-    return (new Date()).getFullYear();
-  }
+    setTranslationLanguage(language) {
+        this.translate.use(language);
+    }
 }
