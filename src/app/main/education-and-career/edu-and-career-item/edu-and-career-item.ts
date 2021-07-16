503e94c5b4977b869d097d12b0e1249e4953bbb9
Mihai Georgevici <mihai@titlecapture.com>
2021-07-16 19:44:28 +0300

    Moved edu and career data in json file

--- src/app/main/education-and-career/edu-and-career-item/edu-and-career-item.ts
+++ src/app/main/education-and-career/edu-and-career-item/edu-and-career-item.ts
@@ -2,7 +2,8 @@ import { EduAndCareerItemPeriod } from './edu-and-career-item-period';
 import { EduAndCareerItemContact } from './edu-and-career-item-contact';
 
 export class EduAndCareerItem {
-    constructor(public title: string,
+    constructor(public id: number,
+                public title: string,
                 public description: string,
                 public period: EduAndCareerItemPeriod,
                 public contact: EduAndCareerItemContact,
